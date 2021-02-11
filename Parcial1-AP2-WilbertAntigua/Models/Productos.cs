8268456e558a9476bc003a4c272096661d4aacd0
Wilbert Smith Antigua Castillo <wilbert_antigua@ucne.edu.do>
2021-02-10 18:40:34 -0600

    Models Hecha

---
+++ Parcial1-AP2-WilbertAntigua/Models/Productos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial1_AP2_WilbertAntigua.Models
+{
+    public class Productos
+    {
+        [Key]
+        public int ProductoId { get; set; }
+
+        [Required(ErrorMessage ="Este campo es obligatorio")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public int Existencia { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public decimal Costo { get; set; }
+
+        public decimal ValorInvertario { get; set; }
+
+    }
+}
