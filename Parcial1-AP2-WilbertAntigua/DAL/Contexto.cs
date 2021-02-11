1ecd9bc9f9b8f38488faf4c2a0ec53818bf2c857
Wilbert Smith Antigua Castillo <wilbert_antigua@ucne.edu.do>
2021-02-10 18:52:06 -0600

    Contexto Listo

---
+++ Parcial1-AP2-WilbertAntigua/DAL/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial1_AP2_WilbertAntigua.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial1_AP2_WilbertAntigua.DAL
+{
+    public class Contexto : DbContext
+    { 
+        
+        public DbSet< Productos> productos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@" Data Source=DATA\ProductoDB.db");
+        }
+    }
+}
